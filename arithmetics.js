2f17c8e0a193938530656ca3d2ed547589444bdf
El-Kings <134776097+El-Kings@users.noreply.github.com>
2023-06-15 11:36:57 -0700

    Add files via upload

---
+++ arithmetics.js
@@ -0,0 +1,6 @@
+console.log(2+4) // addition
+console.log(7-5) // substraction
+console.log(5*9) // multiplication
+console.log(9/3) // division
+console.log(6%2) // modulus - finding a reminder
+console.log(3**2) //exponential 3**2 == 3*3
---
+++ intro4.js
@@ -0,0 +1,3 @@
+console.log('Welcome to JavaScript') // comments makes code readable
+/*comments makes code readable, 
+easy to reuse and informative*/
