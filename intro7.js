18d71fbbf98ff34190c14d23cb86a439bdb64aa5
El-Kings <134776097+El-Kings@users.noreply.github.com>
2023-06-17 11:03:55 -0700

    Add files via upload

---
+++ intro7.js
@@ -0,0 +1,13 @@
+let age = 74
+const gravity = 9.81
+let mass = 65
+const PI = 3.14
+const boilingPoint = 100
+const bodyTemp = 37
+
+console.log("age:", age);
+console.log("gravity:", gravity);
+console.log("mass:", mass);
+console.log("PI:", PI);
+console.log("boilingPoint:", boilingPoint);
+console.log("bodyTemp:", bodyTemp);
---
+++ intro8.js
@@ -0,0 +1,18 @@
+// addition
+var additionResult = 2 + 4;
+console.log("addition (2+4) =", additionResult);
+// subtraction
+var subtractionResult = 7 - 5;
+console.log("subtraction (7-5) =", subtractionResult);
+//multiplication
+var multiplicationResult = 5 * 9;
+console.log("multiplication(5*9) =", multiplicationResult);
+//division
+var divisionResult = 9 / 3;
+console.log("division (9/3) =", divisionResult);
+//modulus
+var modulusResult = 6 % 3;
+console.log("modulus (6%3) =", modulusResult);
+//exponential
+var exponentialResult = Math.pow(3, 2);
+console.log("exponential (3**2) =", exponentialResult);
